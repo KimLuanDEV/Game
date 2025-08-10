4d0346663476df39c3c0ef4d20b9306811d1c797
KimLuanDev <Troll.Jayd@gmail.com>
2025-08-10 15:18:55 +0700

    Bổ sung

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Cho phép truy cập file tĩnh (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, "public")));
 
-// API test
-app.get("/api/test", (req, res) => {
-    res.json({ message: "Server hoạt động OK" });
-});
-
 app.listen(PORT, () => {
-    console.log(`Server đang chạy tại cổng ${PORT}`);
+    console.log(`Server đang chạy trên cổng ${PORT}`);
 });
